96720aa38b324032e10f3539ee48ca6fded92e80
munal5923 <muhammad08164836426@gmail.com>
2025-04-17 11:40:14 +0100

    Complete Project

---
+++ src/utils.py
@@ -0,0 +1,46 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+import time
+
+
+#Extract Data From the PDF File
+def load_pdf_file(data):
+    loader= DirectoryLoader(data,
+                            glob="*.pdf",
+                            loader_cls=PyPDFLoader)
+
+    documents=loader.load()
+
+    return documents
+
+
+
+#Split the Data into Text Chunks
+def text_split(extracted_data):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks=text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+
+
+#Download the Embeddings from HuggingFace 
+def download_hugging_face_embeddings():
+    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  #this model return 384 dimensions
+    return embeddings
+
+
+def simulate_streaming_response(text, delay=0.05):
+    """
+    Simulates streaming output by yielding one word at a time.
+    
+    Args:
+        text (str): The full text to stream.
+        delay (float): Time delay between words.
+    
+    Yields:
+        str: The next word in the stream.
+    """
+    for word in text.split():
+        yield word + " "
+        time.sleep(delay)
